76b94286b1e64702d720b6a3fd6af3b18585a1cc
khouloud <khouloudwalid24@gmail.com>
2022-07-25 23:47:57 +0200

    projet finalisé

--- back/app.js
+++ back/app.js
@@ -15,7 +15,7 @@ const userRoutes = require('./routes/users');
 const sauceRoutes = require('./routes/sauces');
 
 const mongoose = require('mongoose');
-mongoose.connect(process.env.DB_CONNECT,
+mongoose.connect('mongodb+srv://${process.env.USER}:${process.env.SECRET_KEY}@${process.env.DB_CLUSTER}.cwdvu.mongodb.net/?retryWrites=true&w=majority', 
   { useNewUrlParser: true,
     useUnifiedTopology: true })
   .then(() => console.log('Connexion à MongoDB réussie !'))
